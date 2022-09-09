733790063bd0c034ab72059d34806ecc1f13f60d
Hermosita <Helen-aquarius@yandex.ru>
2022-09-09 22:32:20 +0300

    add problem2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
